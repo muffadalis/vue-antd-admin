913d088776b1dd94ef5df7524ddd60642a269aa4
muffadaljaorawala <muffadal.jaorawala@clearpoint.co.nz>
2019-01-17 13:19:07 +1300

    Added dummy insurer list

--- src/insurer/list.vue
+++ src/insurer/list.vue
@@ -1,3 +1,50 @@
+// eslint space-before-function-paren: "off"
 <template>
-    <h1>Insurer List</h1>
+  <div>
+    <a-table
+      style="margin-bottom: 24px"
+      :columns="columns"
+      :dataSource="insurersList"
+      :pagination="false"
+    >
+      <span slot="isActive" slot-scope="text, record">
+        <a-tag v-if="isActive" color="#87d068">Active</a-tag>
+        <a-tag v-if="!isActive" color="#f50">Active</a-tag>
+      </span>
+    </a-table>
+  </div>
 </template>
+
+<script>
+const columns = [
+  {
+    title: 'Code',
+    dataIndex: 'code',
+    key: 'code'
+  },
+  {
+    title: 'Name',
+    dataIndex: 'name'
+  },
+  {
+    title: 'Status',
+    dataIndex: 'isActive',
+    scopedSlots: { customRender: 'isActive' }
+  }
+]
+
+const insurersList = [
+  { code: 'IAG', name: 'IAG Insurance', isActive: true },
+  { code: 'QBE', name: 'QBE Insurance', isActive: false }
+]
+
+export default {
+  name: 'InsurerList',
+  data() {
+    return {
+      columns,
+      insurersList
+    }
+  }
+}
+</script>
